91a3d049cbcbbaf12884d209dfb6c321e2f18523
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-19 07:45:41 +0900

    first

--- submit_stats.php
+++
@@ -1,9 +0,0 @@
-<!-- input_stats.php の一部 -->
-<form action="submit_stats.php" method="post">
-    <label for="player_id">選手ID:</label>
-    <input type="number" name="player_id" required><br>
-    <label for="game_date">試合日:</label>
-    <input type="date" name="game_date" required><br>
-    <!-- 他の項目も追加 -->
-    <input type="submit" value="スタッツを送信">
-</form>
