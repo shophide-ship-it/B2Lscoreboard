d74ca32ff80319c51837696f768a088d36ae53d3
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-18 16:52:19 +0900

    pdo

--- db/db.php
+++ db/db.php
@@ -4,6 +4,9 @@ $dbname = 'kasugai-sp_b2l-league';
 $username = 'kasugai-sp_b2l-league';
 $password = 'B2L_db2025secure';
 
+include 'db.php';
+/** @var PDO $pdo */ // これを追加するとエディタの警告が消えることが多いです
+$stmt = $pdo->query("SELECT * FROM teams");
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
