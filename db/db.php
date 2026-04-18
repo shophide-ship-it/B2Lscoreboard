235e945c5fdd75420e124830d6f12e3680c010df
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-18 19:49:43 +0900

    add debug

--- db/db.php
+++ db/db.php
@@ -6,6 +6,8 @@ $username = 'kasugai-sp_b2l-league';
 $password = 'B2L_db2025secure';
 
 // 2. データベースに接続する（この処理が終わると $pdo が使えるようになります）
+<?php
+// db.php
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,6 +15,11 @@ try {
     // 接続に失敗した場合はエラーを表示して止める
     die("Could not connect to the database {$dbname} :" . $e->getMessage());
 }
-
+echo "データベースに接続成功"; // 追加
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage();
+    exit();
+}
+?>
 // 注意：ここでは SELECT 文（データの取得）は書かなくてOKです。
 // データの取得は、表示を行う index.php 側でやるのが一般的です。
