35e9bb86db4de0c7c052e9351db65659150e2a14
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-20 14:58:14 +0900

    hush

--- b2l/admin/admin_password.php
+++ b2l/admin/admin_password.php
@@ -2,12 +2,12 @@
 ini_set('display_errors', 1);   // エラーを表示
 error_reporting(E_ALL);           // すべてのエラーを報告
 
-// 例: 存在しない変数を出力しようとする
-echo $undefined_variable;  // ここでエラーが発生します
+// ハッシュ化したいパスワードを設定
+$password = 'X_MJJk5CfDwv4nf'; // 実際のパスワードをここに記入
 
 // パスワードのハッシュ生成
-$password = 'your_password'; // 実際のパスワードを使用
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-echo "Hashed Password: " . $hashed_password; // ハッシュを表示
+// ハッシュを表示
+echo "Hashed Password: " . $hashed_password;
 ?>
