fd786215ccf2e79ba4fc566d4596e63cff91c172
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-20 13:05:36 +0900

    hashed pass

--- b2l/admin/admin_password.php
+++ b2l/admin/admin_password.php
@@ -1,6 +1,3 @@
 <?php
-// admin_password.php
-$password = 'X_MJJk5CfDwv4nf'; // 元のパスワード
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-echo $hashed_password; // ハッシュ化されたパスワードを表示
+phpinfo();
 ?>
