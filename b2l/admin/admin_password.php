f1767792bb1dd2a989e3ae2d5f1f7bf9815b2e78
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-20 14:53:03 +0900

    error

--- b2l/admin/admin_password.php
+++ b2l/admin/admin_password.php
@@ -1,6 +1,13 @@
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 <?php
-phpinfo();
+ini_set('display_errors', 1);   // エラーを表示
+error_reporting(E_ALL);           // すべてのエラーを報告
+
+// 例: 存在しない変数を出力しようとする
+echo $undefined_variable;  // ここでエラーが発生します
+
+// パスワードのハッシュ生成
+$password = 'your_password'; // 実際のパスワードを使用
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+echo "Hashed Password: " . $hashed_password; // ハッシュを表示
 ?>
