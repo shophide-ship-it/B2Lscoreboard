7fb987c593d6f8cc0f7c1f51faf9cc64d86a471c
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-20 15:18:57 +0900

    hush

---
+++ b2l/admin/login_form.php
@@ -0,0 +1,9 @@
+<form method="POST" action="login.php">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    
+    <input type="submit" value="Login">
+</form>
