2de40b72e1dc7fa419ba262e3219a03fc99b0614
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-21 10:31:09 +0900

    fix5

--- b2l/config.php
+++ b2l/config.php
@@ -1,12 +1,12 @@
 function getDB() {
-    $host = 'mysql80.kasugai-sp.sakura.ne.jp';
+    $host = '$host = '127.0.0.1';
     $db   = 'kasugai-sp_b2l-league';
     $user = 'kasugai-sp_b2l-league';
     $pass = 'B2L_db2025secure';
 
     try {
         // オプションを追加して接続を強化
-        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+        $dsn = "mysql:host=$host;dbname=$db;
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_TIMEOUT => 5 // 5秒でタイムアウト
