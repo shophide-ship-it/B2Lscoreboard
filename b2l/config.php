b520a999e0f74724daea23058063d12f4e0718dd
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-21 13:08:30 +0900

    fix

--- b2l/config.php
+++ b2l/config.php
@@ -1,11 +1,10 @@
-define('ADMIN_USERNAME', 'admin');
-define('ADMIN_PASSWORD', password_hash('your_secure_password', PASSWORD_DEFAULT));
-
 <?php
 $host = 'mysql80.kasugai-sp.sakura.ne.jp';
 $dbname = 'kasugai-sp_b2l-league';
 $username = 'kasugai-sp_b2l-league';
 $password = 'B2L_db2025secure';
+define('ADMIN_USERNAME', 'admin');
+define('ADMIN_PASSWORD', password_hash('your_secure_password', PASSWORD_DEFAULT));
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
