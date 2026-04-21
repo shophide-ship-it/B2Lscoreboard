dc72754c48778927d47c63d0bc4f743626623f97
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-21 12:21:43 +0900

    basic

---
+++ b2l/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>B2L League</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>B2L League</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">ホーム</a></li>
+                <li><a href="schedule.php">スケジュール</a></li>
+                <li><a href="teams.php">チーム</a></li>
+                <li><a href="leaders.php">リーダーズ</a></li>
+                <li><a href="standings.php">順位</a></li>
+            </ul>
+        </nav>
+    </header>
