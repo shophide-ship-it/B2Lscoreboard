80b3baf31c7813994421d1173ba94a50a313a398
hide3 <ogihara@mve.biglobe.ne.jp>
2026-04-23 03:06:52 +0900

    fa

--- b2l/index.php
+++ b2l/index.php
@@ -46,6 +46,7 @@ $reboundLeaders = $pdo->query("
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= SITE_NAME ?> - バスケットボールリーグ</title>
     <link rel="stylesheet" href="<?= url('css/style.css') ?>">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
     <header class="main-header">
